7664d60e452071970be4887f13c754fe21bdc90b
silverback000mn <gf211063cc@gmail.com>
2025-06-23 19:50:02 -0300

    Create guardia_ra2.py

---
+++ ia/ra/guardias/guardia_ra2.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaRa2:
+    def cobertura(self):
+        print("[🛡 Guardia_RA2] Cobrindo sobrecarga da IA_RA...")
+        for i in range(2):
+            print(f"[🛡 Guardia_RA2] Execução emergencial #{i+1}")
+            time.sleep(1)
+        print("[🛡 Guardia_RA2] Suporte leve finalizado.")
