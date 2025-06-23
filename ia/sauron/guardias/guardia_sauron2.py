aa82a75f2d32e5eefe8b709e82693f1a78274d5e
silverback000mn <gf211063cc@gmail.com>
2025-06-23 19:52:39 -0300

    Create guardia_sauron2.py

---
+++ ia/sauron/guardias/guardia_sauron2.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaSauron2:
+    def cobertura(self):
+        print("[🛡 Guardia_Sauron2] Cobrindo sobrecarga da IA_Sauron...")
+        for i in range(2):
+            print(f"[🛡 Guardia_Sauron2] Execução emergencial #{i+1}")
+            time.sleep(1)
+        print("[🛡 Guardia_Sauron2] Suporte leve finalizado.")
