1621b0301de095ff064cd007f7d468e2a4e45eee
silverback000mn <gf211063cc@gmail.com>
2025-06-23 19:43:55 -0300

    Create app.py

---
+++ painel_web/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, session
+import os
+
+app = Flask(__name__)
+app.secret_key = "ciberpunk_key_2025"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["usuario"] == "admin" and request.form["senha"] == "anubis":
+            session["auth"] = True
+            return redirect("/verificar")
+    return render_template("login.html")
+
+@app.route("/verificar", methods=["GET", "POST"])
+def verificar():
+    if not session.get("auth"): return redirect("/login")
+    if request.method == "POST":
+        if request.form["token"] == "8429":
+            session["logado"] = True
+            return redirect("/")
+    return render_template("verificar.html")
+
+@app.route("/")
+def home():
+    if not session.get("logado"): return redirect("/login")
+
+    try:
+        with open("logs/supremos/horus.log", "r") as f:
+            logs = f.readlines()
+    except:
+        logs = ["[HORUS] Nenhum log ainda."]
+
+    return render_template("painel.html", logs=logs)
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+if __name__ == "__main__":
+    app.run(debug=True)
