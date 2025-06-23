b4afa788061029ebc83e140a3eab32358b00466e
silverback000mn <gf211063cc@gmail.com>
2025-06-23 20:26:20 -0300

    Create guardia_ra.py

---
+++ inteligencia/guardias/guardia_ra.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaRa:
+    def auxiliar(self):
+        print("[🛡 Guardia_RA] Auxiliando IA_RA...")
+        for i in range(3):
+            print(f"[🛡 Guardia_RA] Recuperação #{i+1}")
+            time.sleep(2)
+        print("[🛡 Guardia_RA] ✅ Suporte concluído.")
