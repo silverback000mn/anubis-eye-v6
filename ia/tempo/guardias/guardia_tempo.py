3d1ff6cedebb0aa39aea100fe6fe46171f435d7c
silverback000mn <gf211063cc@gmail.com>
2025-06-23 19:53:04 -0300

    Create guardia_tempo.py

---
+++ ia/tempo/guardias/guardia_tempo.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaTempo:
+    def auxiliar(self):
+        print("[🛡 Guardia_Tempo] Auxiliando IA_Tempo...")
+        for i in range(3):
+            print(f"[🛡 Guardia_Tempo] Recuperação #{i+1}")
+            time.sleep(2)
+        print("[🛡 Guardia_Tempo] Suporte concluído.")
