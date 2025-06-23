668054a7119885617927175ec4ba018fdf486748
silverback000mn <gf211063cc@gmail.com>
2025-06-23 20:28:04 -0300

    Create guardia_isis.py

---
+++ inteligencia/guardias/guardia_isis.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaIsis:
+    def auxiliar(self):
+        print("[🛡 Guardia_Isis] Auxiliando IA_Isis...")
+        for i in range(3):
+            print(f"[🛡 Guardia_Isis] Recuperação #{i+1}")
+            time.sleep(2)
+        print("[🛡 Guardia_Isis] ✅ Suporte concluído.")
