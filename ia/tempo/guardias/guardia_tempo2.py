5535021cb2dfb6621fcc1a3261908ce6d008c97f
silverback000mn <gf211063cc@gmail.com>
2025-06-23 19:53:26 -0300

    Create guardia_tempo2.py

---
+++ ia/tempo/guardias/guardia_tempo2.py
@@ -0,0 +1,9 @@
+import time
+
+class GuardiaTempo2:
+    def cobertura(self):
+        print("[🛡 Guardia_Tempo2] Cobrindo sobrecarga da IA_Tempo...")
+        for i in range(2):
+            print(f"[🛡 Guardia_Tempo2] Execução emergencial #{i+1}")
+            time.sleep(1)
+        print("[🛡 Guardia_Tempo2] Suporte leve finalizado.")
